1cca95ac22502808059a5b69c5ceab4e0518711a
Isaac <aziksimpson@gmail.com>
2025-05-04 21:03:07 +0200

    выводит дату

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup>
-const date = new Date().toLocaleString()
-</script>
-
 <template>
   <h1>{{ date }}</h1>
 </template>
+
+<script setup>
+const date = new Date().toLocaleString()
+</script>
 
 <style scoped></style>
