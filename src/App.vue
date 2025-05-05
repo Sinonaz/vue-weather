1ff150b544dde324456f64a5accf66b31e148218
Isaac <aziksimpson@gmail.com>
2025-05-05 19:10:10 +0200

    форматирует файлы

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import Button from "./components/Button.vue"
+	import Button from './components/Button.vue'
 
-const date = new Date().toLocaleString()
+	const date = new Date().toLocaleString()
 </script>
 
 <template>
-  <h1>{{ date }}</h1>
-  <Button text="Button" />
+	<h1>{{ date }}</h1>
+	<Button text="Button" />
 </template>
 
 <style scoped></style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,32 +1,32 @@
 <script setup>
-defineProps({
-  text: {
-    type: String,
-    required: true,
-  },
-})
+	defineProps({
+		text: {
+			type: String,
+			required: true,
+		},
+	})
 </script>
 
 <template>
-  <button type="button" class="button">{{ text }}</button>
+	<button type="button" class="button">{{ text }}</button>
 </template>
 
 <style scoped>
-.button {
-  padding: 14px;
-  background: var(--gradient);
-  border: none;
-  color: var(--white);
-  font-weight: 600;
-  font-size: 20px;
-  border-radius: 10px;
-  font-family: var(--font-primary);
-  cursor: pointer;
-  transition: background 0.2s;
+	.button {
+		padding: 14px;
+		background: var(--gradient);
+		border: none;
+		color: var(--white);
+		font-weight: 600;
+		font-size: 20px;
+		border-radius: 10px;
+		font-family: var(--font-primary);
+		cursor: pointer;
+		transition: background 0.2s;
 
-  &:hover,
-  &:focus {
-    background: var(--gradient-hover);
-  }
-}
+		&:hover,
+		&:focus {
+			background: var(--gradient-hover);
+		}
+	}
 </style>
